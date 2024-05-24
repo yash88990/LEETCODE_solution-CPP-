d477c2294a60ffa359846f0f3a0ceb49791e53f5
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-05-24 15:44:02 +0530

    Time: 14 ms (75.06%), Space: 25.8 MB (13.36%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> rows(9);
+        vector<unordered_set<char>> columns(9);
+        vector<unordered_set<char>> boxes(9);
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char current = board[i][j];
+                if (current != '.') {
+                    // Check row
+                    if (rows[i].count(current) > 0)
+                        return false;
+                    rows[i].insert(current);
+                    
+                    // Check column
+                    if (columns[j].count(current) > 0)
+                        return false;
+                    columns[j].insert(current);
+                    
+                    // Check 3x3 sub-box
+                    int box_index = (i / 3) * 3 + j / 3;
+                    if (boxes[box_index].count(current) > 0)
+                        return false;
+                    boxes[box_index].insert(current);
+                }
+            }
+        }
+        
+        return true;
+    }
+};
