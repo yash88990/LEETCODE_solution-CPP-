8b031a2e8520c42adb161b6a2e6e63eccaf8515d
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-05-29 07:54:30 +0530

    Time: 0 ms (100%), Space: 8.2 MB (66.38%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int m = num1.size();
+        int n = num2.size();
+        vector<int> result(m + n, 0);
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = mul + result[i + j + 1];
+                result[i + j] += sum / 10;
+                result[i + j + 1] = sum % 10;
+            }
+        }
+
+        string res;
+        for (int num : result) {
+            if (!(res.empty() && num == 0)) {
+                res.push_back(num + '0');
+            }
+        }
+
+        return res.empty() ? "0" : res;
+    }
+};
