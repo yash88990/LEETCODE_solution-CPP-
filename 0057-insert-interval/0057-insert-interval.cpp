ac137b664ff2835ffceb26b115c18c11809b5de0
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-05-29 08:02:48 +0530

    Time: 18 ms (15.39%), Space: 20.5 MB (75.56%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+        int n = intervals.size();
+        int i = 0;
+
+        // Add all intervals that come before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        // Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.push_back(newInterval);
+
+        // Add remaining intervals
+        while (i < n) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        return result;
+    }
+};
