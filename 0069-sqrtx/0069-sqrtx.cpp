d6113ed1d1673e72fd56c461ef8775da393f4695
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-06-01 11:30:53 +0530

    Time: 5 ms (35.76%), Space: 7.4 MB (80.62%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if ( x < 0 ){
+            return false ;
+        }
+        int ans = sqrt(x);
+        return ans ;
+        
+    }
+};
