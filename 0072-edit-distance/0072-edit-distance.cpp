9e8f7f74fb4f55e10509813e5251d40a43b8dfcc
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-06-04 10:38:17 +0530

    Time: 10 ms (40.17%), Space: 11.8 MB (70.84%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        // Initialize a 2D vector dp with size (m+1) x (n+1)
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        
+        // Base case: if one of the words is empty, the minimum distance is the length of the other word
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; ++j) {
+            dp[0][j] = j;
+        }
+        
+        // Fill the dp table
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
