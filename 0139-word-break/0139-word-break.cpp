4a54f67f592c7fda36441d87b9ce073d9c9425dd
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-06-13 07:24:18 +0530

    Time: 18 ms (31.29%), Space: 15.6 MB (50.97%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true; // Base case: an empty string can be segmented
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && wordSet.find(s.substr(j, i - j)) != wordSet.end()) {
+                    dp[i] = true;
+                    break; // No need to check further if dp[i] is true
+                }
+            }
+        }
+        return dp[n];
+    }
+};
