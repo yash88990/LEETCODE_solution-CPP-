46fd3df52a72207d58e750207a22cdf5ce0efdbf
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-06-14 07:58:10 +0530

    Time: 117 ms (68.7%), Space: 69.1 MB (79.13%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -1,18 +1,18 @@
-
 class Solution {
 public:
-    int minIncrementForUnique(std::vector<int>& nums) {
-        if (nums.empty()) return 0;
-        // Sort the array
-        sort(nums.begin(), nums.end());
-        int moves = 0;
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] <= nums[i - 1]) {
-                int increment = nums[i - 1] - nums[i] + 1;
-                nums[i] += increment;
-                moves += increment;
+    int minIncrementForUnique(vector<int>& nums) {
+        if (nums.empty() ) return 0 ;
+        // sort the array 
+        sort(nums.begin() , nums.end());
+        int moves = 0 ;
+        for(int i = 1 ; i < nums.size() ; i++ ){
+            if(nums[i] <= nums[i-1]){
+                int increment = nums[i-1] - nums[i] + 1 ;
+                nums[i] += increment ;
+                moves += increment ;
             }
+
         }
-        return moves;
+        return moves ;
     }
 };
