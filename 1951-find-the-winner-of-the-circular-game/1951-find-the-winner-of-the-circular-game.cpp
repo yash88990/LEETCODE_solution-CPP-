70519a601a2268875ae20e1a7c02362091082a71
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-07-08 21:24:14 +0530

    Time: 2 ms (75.45%), Space: 7.2 MB (76.96%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        int winner = 0; // Start with the base case (Josephus(1, k) = 0)
-        for (int i = 1; i <= n; ++i) {
-            winner = (winner + k) % i;
+        int winner = 0 ; 
+        for(int i = 1 ; i <= n ; i++){
+            winner = (winner + k ) % i;
         }
-        return winner + 1; // Convert from 0-based to 1-based indexing
+        return winner + 1 ;
     }
 };
