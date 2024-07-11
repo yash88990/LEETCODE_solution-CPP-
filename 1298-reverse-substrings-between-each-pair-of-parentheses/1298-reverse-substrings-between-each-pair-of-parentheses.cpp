80c143e74093946d666f0b8bff016fdd6ed32c04
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-07-11 10:46:13 +0530

    Time: 0 ms (100%), Space: 7.4 MB (84.88%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,29 +1,22 @@
-#include <string>
-#include <stack>
-#include <algorithm> // For std::reverse
-
 class Solution {
 public:
-    std::string reverseParentheses(std::string s) {
-        std::stack<int> stack;
-        
-        for (int i = 0; i < s.length(); ++i) {
-            if (s[i] == '(') {
+    string reverseParentheses(string s) {
+        stack<int> stack ;
+        for(int i = 0 ; i < s.length() ; i++ ){
+            if(s[i] == '('){
                 stack.push(i);
-            } else if (s[i] == ')') {
-                int start = stack.top();
+            }else if(s[i] == ')'){
+                int start = stack.top() ;
                 stack.pop();
-                reverse(s.begin() + start + 1, s.begin() + i);
+                reverse(s.begin() + start + 1 , s.begin() + i);
             }
         }
-        
-        string result;
-        for (char c : s) {
-            if (c != '(' && c != ')') {
-                result += c;
+        string result ;
+        for(char c : s ){
+            if(c != '(' && c != ')'){
+                result += c ;
             }
         }
-        
-        return result;
+        return result ;
     }
 };
