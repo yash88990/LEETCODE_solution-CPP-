aff30ccc15f330a7500ef41076ddee938fa51060
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-09-10 00:52:55 +0530

    Time: 85 ms (17.49%), Space: 70.5 MB (88.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,10 +5,9 @@ public:
         int maxi = nums[0];
         for(int i = 0 ; i < nums.size() ; i++ ){
             sum += nums[i];
-            maxi = max(maxi , sum );
-            if ( sum < 0 ) sum = 0 ;
+            maxi = max( sum , maxi );
+            if (sum < 0 ) sum = 0;
         }
-        return maxi ;
-        
+        return maxi;
     }
 };
