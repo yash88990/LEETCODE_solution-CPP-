2ae4b7bea29a638ab43971b7c7c4687a043dcfb9
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-09-14 22:35:51 +0530

    Time: 10 ms (37.8%), Space: 21.2 MB (85.88%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,7 +5,9 @@ public:
         int index=0;
         for(int i = 0 ; i < n ; i++){
             if(nums[index] != nums[i]){
-                nums[++index] = nums[i];
+                index++;
+                nums[index] = nums[i];
+                
             }
         }
         return index+1;
