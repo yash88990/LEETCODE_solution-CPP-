d85985fb8ecd82eeb4622931119aed0697470aed
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-09-18 13:46:39 +0530

    Time: 5 ms (35.33%), Space: 8.3 MB (72.78%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if ( x < 0 ){
-            return false ;
+        int s = 0 , e = x ; 
+        long long int ans = -1 ;
+        long long int mid = s + ( e - s ) / 2 ;
+        while ( s <= e ){
+            long long int square = mid * mid ;
+            if ( square == x ){
+                return mid ;
+            }
+
+            if ( square < x ){
+                ans = mid ;
+                s = mid + 1 ;
+            }else{
+                e = mid - 1 ;
+            }
+            mid =  s + ( e - s ) /  2;
+
         }
-        int ans = sqrt(x);
         return ans ;
-        
+
     }
 };
