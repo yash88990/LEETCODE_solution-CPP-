895fe97216440b518a64f4c7ea77c5832e117200
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-04 18:00:44 +0530

    Time: 2 ms (12.03%), Space: 9.4 MB (15.33%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
         int n = s.size();
-        for(int i = 0 ; i < n ; i += 2 * k ){
-            int end  = min(i + k - 1 , n - 1 );
+        for(int i = 0 ; i < n  ; i += 2 * k ){
+            int end = min(i + k - 1 , n - 1 );
             reverse(s.begin() + i , s.begin() + end + 1 );
-            
         }
         return s ;
     }
