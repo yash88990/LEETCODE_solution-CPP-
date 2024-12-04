35780e7d5154ee12c9f296f46f1f459bf0d5e4bb
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-04 18:45:01 +0530

    Time: 0 ms (100%), Space: 12.1 MB (53.05%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,20 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     string reverseWords(string s) {
         int n = s.size();
-        int start = 0;
-
-        for (int i = 0; i <= n; ++i) {
-            // When a space or the end of the string is encountered
-            if (i == n || s[i] == ' ') {
-                reverse(s.begin() + start, s.begin() + i); // Reverse the word
-                start = i + 1; // Move to the start of the next word
+        int start = 0 ;
+        for(int i = 0 ; i <= n ; i++ ){
+            if(i == n || s[i] == ' ' ){
+                reverse(s.begin() + start , s.begin() + i );
+                start = i + 1 ;
             }
         }
-
         return s;
     }
 };
