e7da2ea3fa346338cad2983e43585923a0a3e6b3
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-11 23:11:14 +0530

    Time: 33 ms (60.78%), Space: 61.4 MB (62.49%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,22 +1,14 @@
-#include <vector>
-#include <queue>
-
 class Solution {
 public:
-    int findKthLargest(std::vector<int>& nums, int k) {
-        // Min-heap banate hain
-      priority_queue<int, vector<int>,greater<int>> minHeap;
-        
-        // Har element ko check karte hain
-        for (int num : nums) {
-            minHeap.push(num);  // Element ko heap me dalte hain
-            // Agar heap ka size k se zyada ho gaya, toh smallest element ko nikaal dete hain
-            if (minHeap.size() > k) {
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int , vector<int>,greater<int>> minHeap;
+        for(int num : nums){
+            minHeap.push(num);
+
+            if(minHeap.size() > k ){
                 minHeap.pop();
             }
         }
-        
-        // Ab heap ke top pe jo element hai, woh kth largest element hoga
         return minHeap.top();
     }
 };
