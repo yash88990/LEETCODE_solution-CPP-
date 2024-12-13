fd61eed6a946b5d97e7bf4e503c6e725f769e941
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-13 14:06:17 +0530

    Time: 0 ms (100%), Space: 10.6 MB (39.63%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> nums;
-        int num = 0;
-        int result = 0;
-        int sign = 1; // 1 for positive, -1 for negative
+        stack<int> stk;
+        int currentResult = 0;  // Store the current result
+        int sign = 1;           // 1 for positive, -1 for negative
+        int i = 0;
 
-        for (char c : s) {
-            if (isdigit(c)) {
-                num = num * 10 + (c - '0');
-            } else if (c == '+' || c == '-') {
-                result += sign * num;
-                num = 0;
-                sign = (c == '+') ? 1 : -1;
-            } else if (c == '(') {
-                nums.push(result);
-                nums.push(sign);
-                result = 0;
-                sign = 1;
-            } else if (c == ')') {
-                result += sign * num;
-                num = 0;
-                result *= nums.top(); // Previous sign
-                nums.pop();
-                result += nums.top(); // Previous result
-                nums.pop();
+        while (i < s.size()) {
+            char ch = s[i];
+            if (isdigit(ch)) {
+                int num = 0;
+                while (i < s.size() && isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+                currentResult += sign * num;  // Apply the sign to the number
+                i--;  // Adjust index because the loop will increment it
+            } else if (ch == '+') {
+                sign = 1;  // Set positive sign
+            } else if (ch == '-') {
+                sign = -1;  // Set negative sign
+            } else if (ch == '(') {
+                stk.push(currentResult);  // Save the current result
+                stk.push(sign);           // Save the current sign
+                currentResult = 0;        // Reset for new sub-expression
+                sign = 1;                 // Default positive sign
+            } else if (ch == ')') {
+                int prevSign = stk.top(); stk.pop();
+                int prevResult = stk.top(); stk.pop();
+                currentResult = prevResult + prevSign * currentResult;
             }
+            i++;  // Move to the next character
         }
 
-        result += sign * num; // Add the last number
-
-        return result;
+        return currentResult;
     }
 };
