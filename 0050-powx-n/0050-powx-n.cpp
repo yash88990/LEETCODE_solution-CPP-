fb4b4a3fb5b13c26014350ae80fd1183144d05e9
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-14 23:13:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // Base case: any number raised to the power of 0 is 1
-        if (n == 0) return 1.0;
-
-        // Handle the special case where n == INT_MIN
-       if (n == INT_MIN) { 
-        // Special case to handle INT_MIN since -n overflows
-        return 1 / (myPow(x, -(n + 1)) * x);
-    }
-
-        // If n is negative, calculate the positive power and take the reciprocal
-        if (n < 0) {
-            return 1 / myPow(x, -n);
-        }
-
-        // Recursive case: use exponentiation by squaring
-        double half = myPow(x, n / 2);
-        if (n % 2 == 0) {
-            return half * half; // If n is even
-        } else {
-            return half * half * x; // If n is odd
-        }
+        //base case 
+        if(n == 0 ) return 1.0;
+        if(n == INT_MIN) return 1/myPow(x , -(n + 1) ) * x;
+        if(n < 0 ) return 1/myPow(x , -n);
+        double half = myPow(x , n/2);
+        if(n % 2 == 0 ) return half * half;
+        else return half * half * x;
     }
 };
