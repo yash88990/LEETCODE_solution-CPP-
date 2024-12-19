b00fcb40398a6813698886eae4e4315f97e10467
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-20 00:51:17 +0530

    Time: 0 ms (100%), Space: 10.5 MB (47.81%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,43 +1,39 @@
-#include <stack>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     int calculate(string s) {
         int result = 0;
-        int sign = 1; // +1 for positive, -1 for negative
+        int sign = 1;
         stack<int> st;
-        int n = s.length();
         int i = 0;
-
-        while (i < n) {
+        while(i <  s.size()){
             char c = s[i];
-            if (isdigit(c)) {
-                int num = 0;
-                while (i < n && isdigit(s[i])) {
+            if(isdigit(c)){
+                int num = 0 ;
+                while(i < s.size() && isdigit(s[i])){
                     num = num * 10 + (s[i] - '0');
                     i++;
                 }
-                result += sign * num; // Add the parsed number to the result
+                result += sign * num;
                 continue;
-            } else if (c == '+') {
-                sign = 1;
-            } else if (c == '-') {
+            }else if ( c == '+'){
+                sign = 1 ;
+            }else if (c == '-') {
                 sign = -1;
-            } else if (c == '(') {
-                st.push(result); // Save the current result
-                st.push(sign);   // Save the current sign
-                result = 0;      // Reset result for the new sub-expression
-                sign = 1;        // Reset sign
-            } else if (c == ')') {
-                result = st.top() * result; // Multiply by the sign before the '('
+            }else if ( c == '('){
+                st.push(result);
+                st.push(sign);
+                result = 0 ;
+                sign = 1;
+            }
+            else if ( c == ')'){
+                result = st.top() * result ;
                 st.pop();
-                result += st.top(); // Add the result before the '('
+                result += st.top();
                 st.pop();
+
             }
             i++;
         }
-        return result;
+        return result ;
     }
 };
