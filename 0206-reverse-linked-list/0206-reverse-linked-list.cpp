d3f85720f4ad3945ba5d4d4ee29266ee87ee3c3f
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-19 21:36:52 +0530

    Time: 0 ms (100%), Space: 13.4 MB (13.51%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,21 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        // Agar list empty hai ya sirf ek node hai, to wahi list return karo
-        if (!head || !head->next) return head;
-
-        ListNode* prev = nullptr; // Pehle pointer null pe set karte hain
-        ListNode* curr = head;    // Current pointer head pe set karte hain
-
-        // Jab tak current null nahi ho jata, list ko reverse karte hain
-        while (curr) {
-            ListNode* nextNode = curr->next; // Next node ko store karte hain
-            curr->next = prev;              // Current ka next pichle node pe point kare
-            prev = curr;                    // Prev pointer ko current node pe le aao
-            curr = nextNode;                // Current pointer ko next node pe le aao
+        if(!head || head->next == NULL) return head;
+        ListNode* prev = NULL;
+        ListNode* curr = head ;
+        while(curr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr = temp;
         }
-
-        // Last me prev pointer nayi head ban jayegi
-        return prev;
+        return prev ;
     }
 };
