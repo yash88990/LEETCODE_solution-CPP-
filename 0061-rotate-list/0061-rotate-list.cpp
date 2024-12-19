bb95d5fb8300f6b60d75e259ff7e9a9f252e0c41
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-19 21:20:53 +0530

    Time: 0 ms (100%), Space: 16.4 MB (72.69%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,33 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (!head || !head->next || k == 0) {
-            return head;
-        }
-        
-        // Calculate the length of the list
-        ListNode* temp = head;
-        int length = 1; // Since we are already at the head node
-        while (temp->next) {
-            temp = temp->next;
+        if(!head || !head->next || k == 0 ) return head;
+        //linked list ka length 
+        ListNode* curr = head ;
+        int length = 1;
+
+        while(curr->next){
+            curr = curr->next;
             length++;
         }
-        
-        // Connect the last node to the head to make it a circular list
-        temp->next = head;
-        
-        // Find the new tail: (length - k % length - 1)th node
-        // and the new head: (length - k % length)th node
-        k = k % length;
-        int stepsToNewHead = length - k;
+        k = k % length ;
+        if(k ==0 ) return head;
+        curr->next = head ;
+        int stepToNewHead = length - k ;
         ListNode* newTail = head;
-        for (int i = 1; i < stepsToNewHead; i++) {
+        for(int i = 1 ; i < stepToNewHead ; i++){
             newTail = newTail->next;
         }
-        
         ListNode* newHead = newTail->next;
-        newTail->next = nullptr;
-        
+        newTail->next = NULL;
         return newHead;
     }
 };
