bf549644c5f27f98670fecfb8b711e588c0c3f5a
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-19 21:44:16 +0530

    Time: 0 ms (100%), Space: 15.8 MB (25.01%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,34 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        // Agar list empty hai ya sirf ek node hai, to wahi return karo
-        if (!head || !head->next) return head;
-
-        // Ek temporary dummy node create karte hain, jo head se pehle point karegi
-        ListNode* temp = new ListNode(0, head);
-
-        // Prev pointer dummy node pe set karo
+        if(!head || !head->next) return head;
+        ListNode* temp = new ListNode(0 , head);
         ListNode* prev = temp;
+        while(head){
+            if(head->next && head->val == head->next->val){
 
-        // Current node traverse karne ke liye
-        while (head) {
-            // Agar current node aur uske next node ka value same hai
-            if (head->next && head->val == head->next->val) {
-                // Jab tak same values milti hain, aage badhte raho
-                while (head->next && head->val == head->next->val) {
-                    head = head->next; // Same value wale nodes skip karo
+                while(head->next && head->val == head->next->val){
+                    head=head->next;
                 }
-                // Prev ka next ko next distinct node par set karo
                 prev->next = head->next;
-            } else {
-                // Agar current node unique hai, prev ko aage badhao
-                prev = prev->next;
-            }
-            // Head pointer ko aage badhao
-            head = head->next;
+            }else{
+            prev = prev->next;
         }
-
-        // Dummy node ke next ko return karo jo nayi list ka head hai
+        head = head->next;
+        }
         return temp->next;
     }
 };
