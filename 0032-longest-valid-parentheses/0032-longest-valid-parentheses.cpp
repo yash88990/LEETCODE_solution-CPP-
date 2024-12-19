9f351c72155716038071fb49870c99b04e079ee7
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-20 00:41:22 +0530

    Time: 2 ms (35.4%), Space: 10.4 MB (26.21%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,27 +1,18 @@
-#include <stack>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> stack;
-        stack.push(-1); // Base case to handle edge cases
-        int maxLen = 0;
-
-        for (int i = 0; i < s.length(); ++i) {
-            if (s[i] == '(') {
-                stack.push(i); // Push index of '('
-            } else {
-                stack.pop(); // Pop the top (matching '(' or base case `-1`)
-                if (stack.empty()) {
-                    stack.push(i); // Push current index as a new base
-                } else {
-                    maxLen = max(maxLen, i - stack.top()); // Update max length
-                }
+        stack<int> st;
+        st.push(-1);
+        int maxlen = 0;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '('){
+                st.push(i);
+            }else{
+               st.pop();
+               if(st.empty()) st.push(i);
+               else maxlen = max(maxlen , i - st.top());
             }
         }
-
-        return maxLen;
+        return maxlen;
     }
 };
