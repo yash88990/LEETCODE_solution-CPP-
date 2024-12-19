5fc3656904469adb373756b203ad7cb95052caf9
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-20 00:58:40 +0530

    Time: 8 ms (52.92%), Space: 15.1 MB (23.32%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,48 +1,36 @@
-#include <stack>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     int calculate(string s) {
         stack<int> st;
-        char lastOp = '+'; // To track the last operator
-        int num = 0;       // To parse numbers
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
+        char lastop = '+';
+        int num = 0;
+        int n = s.size();
+        for(int i = 0 ; i < n ; i++){
             char c = s[i];
-            
-            if (isdigit(c)) {
-                num = num * 10 + (c - '0'); // Parse the number
-            }
-            
-            if ((!isdigit(c) && !isspace(c)) || i == n - 1) {
-                if (lastOp == '+') {
-                    st.push(num);
-                } else if (lastOp == '-') {
-                    st.push(-num);
-                } else if (lastOp == '*') {
+            if(isdigit(c)) num = num * 10 + ( c - '0');
+            if((!isdigit(c) && !isspace(c)) || i == n - 1 ){
+                if(lastop == '+')st.push(num);
+                else if ( lastop == '-') st.push(-num);
+                else if (lastop == '*'){
                     int top = st.top();
                     st.pop();
                     st.push(top * num);
-                } else if (lastOp == '/') {
+                }else if (lastop == '/'){
                     int top = st.top();
                     st.pop();
                     st.push(top / num);
                 }
-                lastOp = c; // Update the last operator
-                num = 0;    // Reset the number
-            }
-        }
+                lastop = c;
+                num = 0;
 
-        // Sum up all values in the stack
+            }
+
+        }
         int result = 0;
-        while (!st.empty()) {
+        while(!st.empty()){
             result += st.top();
             st.pop();
         }
-
-        return result;
+        return result ;
     }
 };
