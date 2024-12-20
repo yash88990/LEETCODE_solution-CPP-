c4489ee0bbada92d8b1a06e1a71de55f61f3d5b6
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-20 13:35:25 +0530

    Time: 0 ms (100%), Space: 9.5 MB (7.74%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,43 +1,38 @@
-#include <queue>
-using namespace std;
-
 class MyStack {
-private:
-    queue<int> q1; // Main queue
-    queue<int> q2; // Helper queue
+    private:
+    queue<int> q1 ; // main queue
+    queue<int> q2;  // temp  queue
 
 public:
     MyStack() {
-        // Constructor mein kuch nahi karna
+        
     }
-
+    
     void push(int x) {
-        // Element ko helper queue (q2) mein daalo
         q2.push(x);
-        // Main queue ke saare elements helper queue mein daalo
-        while (!q1.empty()) {
+        while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
-        // Helper queue ko main queue bana do
-        swap(q1, q2);
+        swap(q1 , q2);
+        
     }
-
+    
     int pop() {
-        // Top element ko remove karna
-        int topElement = q1.front();
+        int topelement = q1.front();
         q1.pop();
-        return topElement;
+        return topelement;
+        
     }
-
+    
     int top() {
-        // Top element ko return karna
         return q1.front();
+        
     }
-
+    
     bool empty() {
-        // Check karo ki queue empty hai ya nahi
         return q1.empty();
+        
     }
 };
 
