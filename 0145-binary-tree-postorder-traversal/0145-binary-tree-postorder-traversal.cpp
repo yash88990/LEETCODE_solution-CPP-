9fd2899a9a2009eb3ee4a8360899684c656cf556
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-21 00:22:27 +0530

    Time: 0 ms (100%), Space: 11.1 MB (22.24%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,22 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<int> result;  // Vector to store the result
-
-    void postorder(TreeNode* node) {
-        if (node == nullptr) return;
-
-        // First recur on the left subtree
-        postorder(node->left);
-
-        // Then recur on the right subtree
-        postorder(node->right);
-
-        // Finally, visit the root node
-        result.push_back(node->val);
-    }
-
+void post(TreeNode* root , vector<int>& result){
+    //base case 
+    if(root == NULL ) return ;
+    post(root->left , result);
+    post(root->right , result);
+    result.push_back(root->val);
+}
     vector<int> postorderTraversal(TreeNode* root) {
-        postorder(root);  // Start the recursive traversal
+        vector<int> result;
+        post(root , result);
         return result;
+        
     }
 };
