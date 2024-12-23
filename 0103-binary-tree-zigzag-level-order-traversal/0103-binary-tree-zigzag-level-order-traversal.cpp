931fdf240df1f03c03ea7fd20590945c4929a5fb
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-24 00:14:58 +0530

    Time: 0 ms (100%), Space: 15.3 MB (8.46%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,34 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        if (root == NULL) return result;
-
+        if(root == NULL ) return result;
         queue<TreeNode*> q;
-        q.push(root);  // Push the root node into the queue
+        q.push(root);
         bool leftToRight = true;
-
-        while (!q.empty()) {
+        while(!q.empty()){
             int size = q.size();
-            vector<int> ans(size);  // Create a vector for the current level
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* frontNode = q.front();
+            vector<int> ans(size);
+            for(int i = 0 ; i < size ; i++){
+                TreeNode* frontnode = q.front();
                 q.pop();
-
-                // Determine the correct index based on the direction
-                int index = leftToRight ? i : size - i - 1;
-                ans[index] = frontNode->val;
-
-                // Enqueue child nodes
-                if (frontNode->left) q.push(frontNode->left);
-                if (frontNode->right) q.push(frontNode->right);
+                int index = leftToRight ? i : size - i - 1 ;
+                ans[index] = frontnode->val;
+                if(frontnode->left) q.push(frontnode->left);
+                if(frontnode->right) q.push(frontnode->right);
             }
+            result.push_back(ans);
+            leftToRight = !leftToRight;
 
-            result.push_back(ans);  // Add the current level to the result
-            leftToRight = !leftToRight;  // Toggle the direction
         }
-
         return result;
     }
 };
