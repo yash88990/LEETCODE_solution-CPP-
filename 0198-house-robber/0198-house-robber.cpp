fba3855f016fa0543ddcc3820b62a1287b3dd1ad
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-29 23:55:42 +0530

    Time: 0 ms (100%), Space: 10.1 MB (49.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int prev2 = 0, prev1 = 0, curr;
-    for (int num : nums) {
-        curr = max(prev1, prev2 + num);
-        prev2 = prev1;
-        prev1 = curr;
-    }
-    return curr;
+        int prev1 = 0, prev2 = 0 , curr;
+        for(int num : nums){
+            curr = max(prev1 , prev2 + num);
+            prev2 = prev1 ;
+            prev1 = curr;
+        }
+        return curr;
     }
 };
