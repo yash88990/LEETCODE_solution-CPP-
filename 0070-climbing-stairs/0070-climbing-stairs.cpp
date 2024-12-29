622031ebb54f32f24945cb742ec02be5724319c0
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-29 23:53:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (39.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        //base case 
-        if(n <= 1 ) return 1;
-        int prev2 = 1 , prev1 = 1 , curr;
-        for(int  i = 2 ; i <= n ; i++){
-            curr = prev1 + prev2;
+        if(n <= 1 ) return n;
+        int prev1 = 1;
+        int prev2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            int curr = prev1 + prev2;
             prev2 = prev1;
             prev1 = curr;
-
         }
-        return curr;
+        return prev1;
     }
 };
