fdd40b554a73ef75bd41f451fcc9c1202b045f44
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-29 20:52:23 +0530

    Time: 18 ms (84.21%), Space: 18 MB (57.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,15 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // Initialize DP array with amount + 1 (a value greater than the maximum possible)
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0; // Base case: 0 coins needed to make amount 0
-        
-        // Update the DP table
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; ++i) {
+         vector<int> dp(amount + 1, INT_MAX);
+    dp[0] = 0;
+    for (int coin : coins) {
+        for (int i = coin; i <= amount; i++) {
+            if (dp[i - coin] != INT_MAX) {
                 dp[i] = min(dp[i], dp[i - coin] + 1);
             }
         }
-        
-        // If dp[amount] is still amount + 1, it means we can't make that amount with the given coins
-        return dp[amount] > amount ? -1 : dp[amount];
+    }
+    return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
