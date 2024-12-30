f3b102413c16ba31dbe3bf7eee64c282c99f1c6e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2024-12-30 21:24:15 +0530

    Time: 2 ms (53.84%), Space: 9.5 MB (46.97%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-         // Check if lengths of strings are different, return false if so
-        if (s.length() != t.length())
-            return false;
-        
-        // Create two maps to store the mappings of characters
-        std::unordered_map<char, char> s_to_t;
-        std::unordered_map<char, char> t_to_s;
-        
-        // Iterate through each character of the strings
-        for (int i = 0; i < s.length(); ++i) {
-            char s_char = s[i];
-            char t_char = t[i];
-            
-            // If s_char is already mapped to a different character in t
-            if (s_to_t.find(s_char) != s_to_t.end() && s_to_t[s_char] != t_char)
-                return false;
-            
-            // If t_char is already mapped to a different character in s
-            if (t_to_s.find(t_char) != t_to_s.end() && t_to_s[t_char] != s_char)
-                return false;
-            
-            // Update the mappings
-            s_to_t[s_char] = t_char;
-            t_to_s[t_char] = s_char;
+        if(s.size() != t.size()) return false;
+        unordered_map<char , char> mapS  , mapT;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(mapS.find(s[i]) != mapS.end()){
+                if(mapS[s[i]] != t[i]) return false;
+            }else{
+                mapS[s[i]] = t[i];
+            }
+            if(mapT.find(t[i]) != mapT.end()){
+                if(mapT[t[i]] != s[i]) return false;
+            }else{
+                mapT[t[i]] = s[i];
+            }
         }
-        
         return true;
     }
 };
