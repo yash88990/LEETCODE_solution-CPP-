6865bec18498bf4aad8698e949d0936d5714a672
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-01-29 10:59:22 +0530

    Time: 7 ms (18.76%), Space: 14.6 MB (15.94%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+bool dfs(unordered_map<int , vector<int>>& adj , int u , int v , vector<bool>& visited ){
+    visited[u] = true;
+    if(u ==  v) return true;
+    for(int &i : adj[u]){
+        if(visited[i]) continue;
+        if(dfs(adj , i , v , visited)) return true;
+
+    }
+    return false;
+}
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        unordered_map<int , vector<int>> adj;
+        for(int i = 0 ; i< n ;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            vector<bool> visited(n , false);
+            if(adj.find(u) != adj.end()  && adj.find(v) != adj.end() && dfs(adj , u , v , visited )){
+                return edges[i];
+            }
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        return {};
+        
+    }
+};
