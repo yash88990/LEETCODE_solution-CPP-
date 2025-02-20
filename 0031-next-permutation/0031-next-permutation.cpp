bb7235d3411b2b77be7ce4110b31eeda8abd5739
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-21 02:16:21 +0530

    Time: 0 ms (100%), Space: 15.9 MB (4.93%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;
-        
-        // Step 1: Find the first element that is not in ascending order from the right
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        
-        // If no such element is found, reverse the entire array and return
-        if (i < 0) {
-            std::reverse(nums.begin(), nums.end());
-            return;
-        }
-        
-        // Step 2: Find the smallest element to the right of i that is greater than nums[i]
-        int j = n - 1;
-        while (nums[j] <= nums[i]) {
-            j--;
-        }
-        
-        // Step 3: Swap nums[i] and nums[j]
-        std::swap(nums[i], nums[j]);
-        
-        // Step 4: Reverse the sub-array to the right of i
-        std::reverse(nums.begin() + i + 1, nums.end());
+        next_permutation(nums.begin() , nums.end());
     }
 };
