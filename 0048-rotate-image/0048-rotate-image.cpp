ff6a5bd26d5ae8b1978234b240f249f337692708
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-24 15:13:50 +0530

    Time: 0 ms (100%), Space: 10.3 MB (8.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,30 +1,16 @@
-#include <vector>
-
 class Solution {
 public:
-    void rotate(std::vector<std::vector<int>>& matrix) {
+    void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-
-        // Traverse each layer of the matrix
-        for (int layer = 0; layer < n / 2; ++layer) {
-            int first = layer;
-            int last = n - 1 - layer;
-
-            // Iterate through each element in the current layer
-            for (int i = first; i < last; ++i) {
-                int offset = i - first;
-                int top = matrix[first][i];
-
-                // Rotate the elements clockwise
-                // Move left element to top
-                matrix[first][i] = matrix[last - offset][first];
-                // Move bottom element to left
-                matrix[last - offset][first] = matrix[last][last - offset];
-                // Move right element to bottom
-                matrix[last][last - offset] = matrix[i][last];
-                // Move top element to right
-                matrix[i][last] = top;
+        for(int i = 0 ;i < n-1 ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                swap(matrix[i][j] , matrix[j][i]);
             }
+
         }
+        for(int i = 0 ; i< n ; i++){
+            reverse(matrix[i].begin() , matrix[i].end());
+        }
+        
     }
 };
