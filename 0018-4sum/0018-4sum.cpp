67f405a32dc9819c1fdbabe15d538ff5fa841e17
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-24 23:14:29 +0530

    Time: 23 ms (56.86%), Space: 17.5 MB (60.08%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,34 +1,36 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
+        sort(nums.begin(), nums.end());
+
         vector<vector<int>> result;
-        vector<int> sortedNums = nums; // Create a copy of nums
-        sort(sortedNums.begin(), sortedNums.end()); // Sort the copied array
-        for (int i = 0; i < n - 3; ++i) {
-            // Skip duplicates
-            if (i > 0 && sortedNums[i] == sortedNums[i - 1]) continue;
-            for (int j = i + 1; j < n - 2; ++j) {
-                // Skip duplicates
-                if (j > i + 1 && sortedNums[j] == sortedNums[j - 1]) continue;
-                long long left = j + 1;
-                long long right = n - 1;
+        if (n < 4) return result;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; 
+
+                int left = j + 1, right = n - 1;
                 while (left < right) {
-                    long long sum = (long long)sortedNums[i] + (long long)sortedNums[j] + (long long)sortedNums[left] + (long long)sortedNums[right];
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
                     if (sum == target) {
-                        result.push_back({sortedNums[i], sortedNums[j], sortedNums[left], sortedNums[right]});
-                        // Skip duplicates
-                        while (left < right && sortedNums[left] == sortedNums[left + 1]) ++left;
-                        while (left < right && sortedNums[right] == sortedNums[right - 1]) --right;
-                        ++left;
-                        --right;
-                    } else if (sum < target) {
-                        ++left;
-                    } else {
-                        --right;
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--; 
+
+                        left++; 
+                        right--; 
+                    } 
+                    else if (sum < target) {
+                        left++;
+                    } 
+                    else {
+                        right--;
                     }
                 }
             }
