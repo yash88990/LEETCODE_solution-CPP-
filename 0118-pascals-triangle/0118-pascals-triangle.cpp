fecd98caa527757810c8eebe9f6904a289367a8b
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-02-24 21:51:02 +0530

    Time: 2 ms (15.93%), Space: 9.6 MB (64.47%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,9 +4,8 @@ public:
         vector<vector<int>> result;
         for (int i = 0; i < numRows; ++i) {
             vector<int> row(i + 1, 1); 
-            for (int j = 1; j < i; ++j) {
+            for (int j = 1; j < i; ++j) 
                 row[j] = result[i - 1][j - 1] + result[i - 1][j];
-            }
             result.push_back(row);
         }
         return result;
