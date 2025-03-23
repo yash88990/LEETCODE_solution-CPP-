e18aa43762e7c92c7a5afe2ac1ed1a39f7d3cbe0
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-03-23 21:34:52 +0530

    Time: 0 ms (100%), Space: 23.1 MB (67.91%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
         unordered_map<int, int> freq;
-        
-        // Count the frequency of each number
         for (int num : nums) {
             freq[num]++;
         }
-        
-        // Find the maximum frequency
-        int maxFreq = 0;
-        for (auto& [key, value] : freq) {
-            maxFreq = max(maxFreq, value);
+        int maxfreq = 0;
+        for(auto & [element , frequency] : freq){
+            maxfreq = max(maxfreq , frequency);
         }
-        
-        // Sum the frequencies of elements that have the maximum frequency
         int sum = 0;
-        for (auto& [key, value] : freq) {
-            if (value == maxFreq) {
-                sum += value;
+        for(auto &[element , frequency] : freq){
+            if(frequency == maxfreq){
+                sum += frequency;
             }
         }
-        
         return sum;
     }
 };
