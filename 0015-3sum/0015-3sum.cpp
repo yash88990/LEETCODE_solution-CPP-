18c038ced4b93cfbe0721009dfbdb0afe72063e5
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-04-29 23:07:35 +0530

    Time: 51 ms (54.93%), Space: 29.2 MB (52.06%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,28 +3,34 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> result;
-        if (n < 3) return result; 
+        if (n < 3) return result;
         sort(nums.begin(), nums.end());
+        
         for (int i = 0; i < n - 2; ++i) {
-           if (i > 0 && nums[i] == nums[i - 1]) continue; 
-           int left = i + 1;
-           int right = n - 1;
-           while (left < right) {
-               int sum = nums[i] + nums[left] + nums[right];
-               if (sum == 0) {
-                   result.push_back({nums[i], nums[left], nums[right]});
-                while (left < right && nums[left] == nums[left + 1]) ++left;
-                while (left < right && nums[right] == nums[right - 1]) --right;
-                ++left;
-                --right;
-                } else if (sum < 0) {
-                     ++left;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = n - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    while (left < right && nums[left] == nums[left + 1]) ++left;
+                    while (left < right && nums[right] == nums[right - 1]) --right;
+                    
+                    ++left;
+                    --right;
+                }
+                else if (sum < 0) {
+                    ++left;
                 } else {
-                     --right;
+                    --right;
                 }
             }
         }
-
-         return result;  
+        
+        return result;
     }
 };
