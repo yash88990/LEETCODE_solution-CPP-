394ae9593daa0f3df951324f225e9b202ecd7c5a
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-04-29 21:50:20 +0530

    Time: 3 ms (32.62%), Space: 96.7 MB (10.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
+        vector<double> ans;
+        for(int num : nums1){
+            ans.push_back(num);
         }
-        
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        int left = 0, right = m;
-        int halfLen = (m + n + 1) / 2;
-        
-        while (left <= right) {
-            int partitionX = (left + right) / 2;
-            int partitionY = halfLen - partitionX;
-            
-            int maxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-            int maxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-            
-            int minX = (partitionX == m) ? INT_MAX : nums1[partitionX];
-            int minY = (partitionY == n) ? INT_MAX : nums2[partitionY];
-            
-            if (maxX <= minY && maxY <= minX) {
-                if ((m + n) % 2 == 0) {
-                    return (max(maxX, maxY) + min(minX, minY)) / 2.0;
-                } else {
-                    return max(maxX, maxY);
-                }
-            } else if (maxX > minY) {
-                right = partitionX - 1;
-            } else {
-                left = partitionX + 1;
-            }
+         for(int num : nums2){
+            ans.push_back(num);
         }
+        sort(ans.begin() , ans.end());
+        int n = ans.size();
+        int mid = n/2;
+        double res = 0.0;
+        if(n % 2 != 0)return ans[mid];
+        else{
+             res = (ans[mid] + ans[mid - 1]) / 2;
+            
+        } 
+        return res;
         
-        return 0.0;
+            
     }
 };
