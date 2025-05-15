2141797ca5776a5d6f7195e892746b5c2f83143e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-15 18:34:58 +0530

    Time: 0 ms (100%), Space: 30 MB (40.56%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -5,7 +5,7 @@ public:
         vector<string> ans;
         for (int i = 0; i < n; ++i) {
             if (i == 0 || groups[i] != groups[i - 1]) {
-                ans.emplace_back(words[i]);
+                ans.push_back(words[i]);
             }
         }
         return ans;
