eafbc27a66763c30e2f84625ca164125ae3552f4
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-19 10:50:44 +0530

    Time: 0 ms (100%), Space: 10.5 MB (74.03%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int length1 = ( ay2 - ay1 ) ;
-        int width1 = ( ax2 - ax1 ) ;
-        int length2 = ( by2 - by1 ) ;
-        int width2 = ( bx2 - bx1 ) ;
-        int area1 = length1 * width1 ;
-        int area2 = length2 * width2 ;
-        int overlaplength = max ( 0 , min ( ay2 , by2 ) - max ( ay1 , by1 )) ;
-        int overlapwidth = max ( 0 , min(ax2 , bx2 ) - max ( ax1 , bx1  )) ;
-        int overlaparea = overlaplength * overlapwidth ;
-        int totalarea = area1 + area2 - overlaparea ; 
-        return totalarea ;
-        
+        int la = ay2 - ay1;
+        int wa = ax2 - ax1;
+        int lb = by2 - by1 ;
+        int wb = bx2 - bx1; 
+        int aa  = la * wa;
+        int ab  = lb * wb;
+        int overlapl= max( 0 , min(ay2 , by2) - max(ay1 , by1));
+        int overlapw = max( 0 , min(ax2 , bx2) - max(ax1 , bx1));
+        int overlapa = overlapl * overlapw;
+        int totalarea = aa + ab - overlapa ;
+        return totalarea; 
     }
 };
