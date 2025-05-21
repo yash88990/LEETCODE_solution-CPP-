8cbd17e627fbbc0f99b82d21ce2afc5a380a8251
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-21 10:25:15 +0530

    Time: 0 ms (100%), Space: 21.7 MB (69.94%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,10 +4,12 @@ public:
         int actualsum = 0;
         int n = nums.size();
         int expectedsum = n * ( n + 1 ) / 2;
-        for(int num : nums){
-            actualsum += num;
+        for(int i = 0 ; i < n ; i++){
+            actualsum += nums[i];
         }
         return expectedsum - actualsum;
+
+        
 
     }
 };
