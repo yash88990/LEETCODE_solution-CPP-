b0cbab9142f6bb8ec2c896393b3acbaca8fb62a2
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-23 09:43:54 +0530

    Time: 50 ms (57.09%), Space: 29 MB (90.31%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,19 +4,20 @@ public:
         vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin() , nums.end());
-        for(int i = 0 ; i < n-2 ; i++){
+        for(int i = 0 ;i <n-2 ; i++){
             if(i > 0 && nums[i] == nums[i-1])continue;
-            int left = i+ 1 , right = n-1;
+            int left = i+1 , right = n-1;
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-            if(sum == 0){
-                ans.push_back({nums[i] , nums[left] , nums[right]});
-                while(left < right && nums[left] == nums[left + 1])left++;
-                while(left < right && nums[right] == nums[right -  1])right--;
-                left++;
-                right--;
-            }else if( sum < 0)left++;
-            else right--;
+                if(sum == 0){
+                    ans.push_back({nums[i] , nums[left] , nums[right]});
+                    while(left < right && nums[left]  == nums[left + 1 ])left++;
+                    while(left < right && nums[right] == nums[right - 1])right--;
+                    left++;
+                    right--;
+                }
+                else if(sum > 0)right--;
+                else left++;
             }
         }
         return ans;
