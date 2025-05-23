498a0512d6e835557ab04f9a4591d4ed437dec44
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-23 10:31:18 +0530

    Time: 0 ms (100%), Space: 20.3 MB (98.16%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,23 +1,20 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int jump(std::vector<int>& nums) {
+    int jump(vector<int>& nums) {
         int n = nums.size();
+        if(n == 1 )return 0;
         int jumps = 0;
-        int currentEnd = 0; // End of the current jump
-        int farthest = 0; // Farthest point reachable from current jump
-        
-        for (int i = 0; i < n - 1; ++i) {
-            farthest = std::max(farthest, i + nums[i]); // Update the farthest reachable point
-            
-            if (i == currentEnd) { // If we reached the end of current jump
-                ++jumps; // Increment jumps
-                currentEnd = farthest; // Update the end of the current jump
+        int currentend = 0;
+        int farthest = 0;
+
+        for(int i = 0 ; i < n-1 ; i++){
+            farthest = max(farthest , i + nums[i]);
+            if(i == currentend){
+                jumps++;
+                currentend = farthest ;
+                if(currentend >= n- 1)break;
             }
         }
-        
         return jumps;
     }
 };
