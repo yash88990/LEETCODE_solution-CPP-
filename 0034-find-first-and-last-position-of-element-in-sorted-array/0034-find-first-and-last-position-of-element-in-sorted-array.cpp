a241c55aac85ee05b3074441ca114c182e08a6cc
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-25 08:59:02 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,49 @@
 class Solution {
-    int firstocc(vector<int>& nums, int target ){
-        int s = 0 , e = nums.size() - 1 ;
-        int ans = -1 ;
-        while( s <= e ){
-            int mid = s + ( e - s ) /  2;
-            if ( nums[mid] == target ) {
-                ans = mid ;
-                // first occ ke lliye left part me jana hai 
-            e = mid - 1 ;
-            }else if ( nums[mid] > target ){
-                e = mid - 1 ;
-            }else{ // nums[mid] < target 
-            s = mid + 1 ;
+    private:
+    int firstocc(vector<int>& nums, int target){
+        int start = 0 , end = nums.size() - 1;
+        int ans = -1;
+        while(start <= end){
+            int mid = start + ( end - start)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                end = mid - 1;
 
+            }else if(nums[mid] > target){
+                end = mid - 1;
+
+            }else {
+                start = mid + 1;
             }
-            mid = s + ( e - s ) / 2 ;
+
         }
-        return ans ;
+        return ans;
     }
 
-int lastocc(vector<int>& nums, int target){
-    int s = 0 , e = nums.size() -  1;
-    int ans = -1 ;
-    while ( s <= e ){
-        int mid = s + ( e - s ) / 2 ;
-        if ( nums[mid] == target ) {
-            ans = mid ;
-            // last occ ke liye right me jana h 
-            s = mid + 1 ;
-        }else if ( nums[mid] > target ){
-            e = mid - 1 ;
-        }else{
-            s = mid + 1 ;
-        }
-        mid = s + ( e - s ) / 2 ;
-    }
-    return ans ;
-}
+      int lastocc(vector<int>& nums, int target){
+        int start = 0 , end = nums.size() - 1;
+        int ans = -1;
+        while(start <= end){
+            int mid = start + ( end - start)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                start = mid + 1;
 
+            }else if(nums[mid] > target){
+                end = mid - 1;
+
+            }else {
+                start = mid + 1;
+            }
+
+        }
+        return ans;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> p(2);
-        p[0] = firstocc(nums , target );
-        p[1] = lastocc(nums , target );
-        return p ;
+     vector<int> ans(2) ;
+     ans[0] = firstocc(nums , target );
+     ans[1] = lastocc(nums , target);  
+     return ans;
     }
 };
