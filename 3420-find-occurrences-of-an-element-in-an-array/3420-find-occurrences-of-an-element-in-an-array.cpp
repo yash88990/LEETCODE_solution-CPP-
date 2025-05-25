edd71e394c1607f4adf211c7b6ed499eb8c0a8fe
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-25 09:04:08 +0530

    Time: 0 ms (100%), Space: 185.5 MB (60.16%) - LeetHub

--- 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
+++ 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
-        vector<int> xIndices;
-        
-        // Step 1: Store all indices where nums[i] == x
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == x) {
-                xIndices.push_back(i);
+        vector<int>ans;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == x)ans.push_back(i);
+        }
+        vector<int>result;
+        for(int q : queries){
+            if(q <= ans.size()){
+                result.push_back(ans[q - 1]);
+            }else{
+                result.push_back(-1);
             }
         }
-
-        // Step 2: Answer each query
-        vector<int> answer;
-        for (int q : queries) {
-            if (q <= xIndices.size()) {
-                answer.push_back(xIndices[q - 1]); // 1-based occurrence index
-            } else {
-                answer.push_back(-1);
-            }
-        }
-
-        return answer;
+        return result;
     }
 };
