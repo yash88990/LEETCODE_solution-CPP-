9489fbc6fc785eddd5cb2f65e588bbbe7e4642fb
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-26 10:23:36 +0530

    Time: 0 ms (100%), Space: 50.4 MB (74.87%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -3,20 +3,20 @@ public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        int left = 0 , right  = n - 1;
+        int left = 0 , right  = m - 1;
         while(left <= right){
             int mid = left + ( right - left) / 2 ;
             int maxrow = 0;
             for(int i = 1 ; i < n ; i++){
-                if(mat[i][mid] > mat[maxrow][mid] ){
+                if(mat[i] [mid] > mat[maxrow] [mid] ){
                     maxrow = i;
                 }
             }
-            int leftval = (mid - 1 >= 0)? mat[maxrow][mid - 1] : -1;
-            int rightval = (mid + 1 < n) ? mat[maxrow][mid + 1] : -1;
-            if(mat[maxrow][mid] > leftval && mat[maxrow][mid] > rightval){
+            int leftval = (mid - 1 >= 0)? mat[maxrow] [mid - 1] : -1;
+            int rightval = (mid + 1 < m) ? mat[maxrow] [mid + 1] : -1;
+            if(mat[maxrow][mid] > leftval && mat[maxrow] [mid] > rightval){
                 return {maxrow , mid};
-            }else if(leftval > mat[maxrow][mid]){
+            }else if(leftval > mat[maxrow] [mid]){
                 right = mid - 1;
             }else{
                 left = mid + 1;
