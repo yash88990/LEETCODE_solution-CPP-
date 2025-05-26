3f4f232b79a790eaf5e358e2063096ddec266871
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-26 10:20:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        
-        int left = 0, right = n - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            // Find the row with the max element in column `mid`
-            int maxRow = 0;
-            for (int i = 1; i < m; i++) {
-                if (mat[i][mid] > mat[maxRow][mid]) {
-                    maxRow = i;
+        int n = mat.size();
+        int m = mat[0].size();
+        int left = 0 , right  = n - 1;
+        while(left <= right){
+            int mid = left + ( right - left) / 2 ;
+            int maxrow = 0;
+            for(int i = 1 ; i < n ; i++){
+                if(mat[i][mid] > mat[maxrow][mid] ){
+                    maxrow = i;
                 }
             }
-
-            // Check left and right neighbors
-            int leftVal = (mid - 1 >= 0) ? mat[maxRow][mid - 1] : -1;
-            int rightVal = (mid + 1 < n) ? mat[maxRow][mid + 1] : -1;
-
-            if (mat[maxRow][mid] > leftVal && mat[maxRow][mid] > rightVal) {
-                return {maxRow, mid};  // Peak found
-            } else if (leftVal > mat[maxRow][mid]) {
+            int leftval = (mid - 1 >= 0)? mat[maxrow][mid - 1] : -1;
+            int rightval = (mid + 1 < n) ? mat[maxrow][mid + 1] : -1;
+            if(mat[maxrow][mid] > leftval && mat[maxrow][mid] > rightval){
+                return {maxrow , mid};
+            }else if(leftval > mat[maxrow][mid]){
                 right = mid - 1;
-            } else {
+            }else{
                 left = mid + 1;
             }
         }
-
-        return {-1, -1};  // Should never be reached due to guaranteed peak
+        return {-1 ,-1};
     }
 };
