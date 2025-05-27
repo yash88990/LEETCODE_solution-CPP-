3d570abaee5ff5a7ea62044f4f41f9a486ce9659
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-05-27 09:36:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (54.11%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        int start = 0 , end = s.size() - 1 ;
-        while (start < end ){
-            while(start < end && !isalpha(s[start])){
-                start++;
-            }
-            while(start < end && !isalpha(s[end])){
-                end--;
-            }
-            if ( start < end ){
-                swap(s[start++],s[end--]);
+        int start = 0 , end = s.size() - 1;
+        while(start < end){
+            while(start < end && !isalpha(s[start]))start++;
+            while(start < end && !isalpha(s[end]))end--;
+            if(start < end){
+                swap(s[start++] , s[end--]);
             }
         }
-        return s ;
+        return s;
     }
 };
