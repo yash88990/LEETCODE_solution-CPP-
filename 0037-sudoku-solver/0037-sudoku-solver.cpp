b7218a1baf5952b825c114c31c99db620eed199f
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-08-31 21:43:02 +0530

    Time: 375 ms (48.51%), Space: 8.6 MB (99.14%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+    bool isSafe(int row, int col, vector<vector<char>>& board, char val) {
+        for (int i = 0; i < 9; i++) {
+            // check row
+            if (board[row][i] == val) return false;
+            // check column
+            if (board[i][col] == val) return false;
+            // check 3x3 box
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == val) return false;
+        }
+        return true;
     }
-    
-private:
+
     bool solve(vector<vector<char>>& board) {
-        for (int row = 0; row < 9; ++row) {
-            for (int col = 0; col < 9; ++col) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
                 if (board[row][col] == '.') {
-                    for (char num = '1'; num <= '9'; ++num) {
-                        if (isValid(board, row, col, num)) {
-                            board[row][col] = num;
-                            if (solve(board)) {
-                                return true;
-                            }
-                            board[row][col] = '.'; // backtrack
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isSafe(row, col, board, ch)) {
+                            board[row][col] = ch;
+                            if (solve(board)) return true;
+                            board[row][col] = '.'; 
                         }
                     }
-                    return false;
+                    return false;  
                 }
             }
         }
-        return true; // board is fully solved
+        return true;  
     }
-    
-    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
-        for (int i = 0; i < 9; ++i) {
-            if (board[row][i] == num || board[i][col] == num ||
-                board[(row/3) * 3 + i/3][(col/3) * 3 + i%3] == num) {
-                return false;
-            }
-        }
-        return true;
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
     }
 };
