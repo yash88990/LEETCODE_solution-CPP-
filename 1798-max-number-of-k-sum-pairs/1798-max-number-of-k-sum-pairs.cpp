078ed17670d7c51cd50e2c16df25fa4e1e075e56
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-08 11:46:13 +0530

    Time: 123 ms (18.91%), Space: 76.7 MB (24.19%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -3,21 +3,14 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
         int count = 0;
+        for (int num : nums) freq[num]++;
         
-        // Count frequency of each number
-        for (int num : nums) {
-            freq[num]++;
-        }
-        
-        // Check for pairs that sum up to k
         for (int num : nums) {
             if (freq[num] > 0 && freq[k - num] > 0) {
                 if (num == k - num) {
-                    // If both numbers are the same, check if there are enough occurrences to form a pair
                     count += freq[num] / 2;
-                    freq[num] = 0; // Remove the occurrences
+                    freq[num] = 0; 
                 } else {
-                    // Otherwise, take the minimum occurrences between num and k - num
                     count += min(freq[num], freq[k - num]);
                     freq[num] = 0;
                     freq[k - num] = 0;
