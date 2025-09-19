2c4770a5b8c863b529db60096ddd9522a92a0c81
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-19 19:12:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.11%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int solverecc(int n){
+    int solvememo(int n , vector<int>&dp){
         if(n<= 1)return n;
-
-        return solverecc(n-1) + solverecc(n-2); 
-    }
-    int fib(int n) {
-        return solverecc(n);
+        //step 3:- check ans exists or not
+        if(dp[n] != -1)return dp[n];
         
+        //step 2:- store subproblem ans
+        return dp[n] = solvememo(n-1 , dp) + solvememo(n-2 , dp); 
+
+    }
+
+    int fib(int n) {
+        //step 1 :- create a dp arrya of size (n+1)
+        vector<int>dp(n+1 , -1);
+        return solvememo(n , dp);
     }
 };
