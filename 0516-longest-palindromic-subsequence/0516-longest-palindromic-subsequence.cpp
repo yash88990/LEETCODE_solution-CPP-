9f708c78fbb93422a372725ac8f8c645d4f17aa1
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-19 23:09:41 +0530

    Time: 83 ms (50.32%), Space: 76 MB (22.05%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int solve(string a , string b){
-        vector<int> curr(b.length() + 1 , 0);
-        vector<int> next(b.length() +1  , 0);
-        for(int i = a.length() - 1 ; i>= 0; i--){
-            for(int j = b.length() -1 ; j >= 0 ; j--){
-                int ans = 0;
-                if(a[i] == b[j])ans = 1 + next[j+1];
-                else ans = max(next[j] , curr[j+1]);
-                curr[j] = ans;
-            }
-            next = curr;
-        }
-        return next[0];
+    int solve(string& a , string& b , int i , int j , vector<vector<int>>&dp){
+        //base case
+        if( i == a.length() || j == b.length())return 0;
+        int count = 0;
+        if(dp[i][j] != -1)return dp[i][j];
+        if(a[i] == b[j])count = 1 + solve(a, b , i+ 1 , j+1 , dp);
+        else count = max(solve(a ,b, i+1 , j , dp) , solve( a , b , i  ,j+1 , dp));
+        return dp[i][j] = count;
     }
     int longestPalindromeSubseq(string s) {
-        string newstr = s;
-        reverse(newstr.begin() , newstr.end());
-        return solve(s , newstr);
+        string s2 = s;
+        reverse(s2.begin() , s2.end());
+
+        vector<vector<int>>dp(s.length() , vector<int>(s.length() , -1));
+        return solve(s , s2 , 0 , 0 , dp);
+        
     }
 };
