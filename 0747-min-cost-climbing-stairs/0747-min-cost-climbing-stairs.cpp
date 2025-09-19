4818f850decb73cf372f26e15a7a5f59142dda24
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-19 19:34:27 +0530

    Time: 0 ms (100%), Space: 17.7 MB (30.86%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+    int solve(vector<int>&cost , int i , vector<int>&dp ){
+        //base case
+        if( i < 0)return 0;
+        if( i == 0 || i == 1)return cost[i];
+        //step3:- chekc ans is present or not
+        if(dp[i] != -1)return dp[i];
+        //step 2 :- store subproblems ans
+        return dp[i] = cost[i] + min(solve(cost , i-1 , dp) , solve(cost , i-2 , dp));
+    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        if (n == 0) return 0;
-        if (n == 1) return cost[0];
-        
-        int prev2 = cost[0];
-        int prev1 = cost[1];
-        
-        for (int i = 2; i < n; ++i) {
-            int current = cost[i] + min(prev1, prev2);
-            prev2 = prev1;
-            prev1 = current;
-        }
-        
-        return min(prev1, prev2);
+        //step1:- create a dp array
+        vector<int>dp(n+1 , -1);
+        return min(solve(cost , n-1 , dp) , solve(cost , n-2 , dp) );
     }
 };
