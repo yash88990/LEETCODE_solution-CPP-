9672b0c1526b39d902bf3e57c88c6ea31740687f
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-23 19:43:41 +0530

    Time: 3 ms (4.27%), Space: 7.9 MB (99.97%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        vector<int> v1 = splitVersion(version1);
+        vector<int> v2 = splitVersion(version2);
+        
+        int n = max(v1.size(), v2.size());
+        for (int i = 0; i < n; ++i) {
+            int num1 = (i < v1.size()) ? v1[i] : 0;
+            int num2 = (i < v2.size()) ? v2[i] : 0;
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
+        }
+        
+        return 0;
+    }
+    
+private:
+    vector<int> splitVersion(const string& version) {
+        vector<int> revisions;
+        size_t start = 0, end = 0;
+        while ((end = version.find('.', start)) != string::npos) {
+            revisions.push_back(stoi(version.substr(start, end - start)));
+            start = end + 1;
+        }
+        revisions.push_back(stoi(version.substr(start)));
+        return revisions;
+    }
+};
