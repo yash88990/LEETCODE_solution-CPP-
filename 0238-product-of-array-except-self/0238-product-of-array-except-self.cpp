5b64033adb2d0cf93a35535276bbf583944de4ed
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-26 18:04:14 +0530

    Time: 0 ms (100%), Space: 40.2 MB (58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,26 +1,20 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> answer(n, 1);
-
-        // Step 1: Build prefix products
+        vector<int>ans( n , 1);
+        //prefix multiply
         int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            answer[i] = prefix;   // store product of elements before i
-            prefix *= nums[i];    // update prefix
+        for(int i = 0 ; i < n ; i++){
+            ans[i] =  prefix;
+            prefix *= nums[i];
         }
-
-        // Step 2: Multiply with suffix products
         int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= suffix;  // multiply by product of elements after i
-            suffix *= nums[i];    // update suffix
+        for(int i = n-1 ; i >= 0 ; i--){
+            ans[i] *= suffix;
+            suffix *= nums[i];
         }
-
-        return answer;
+        return ans;
+        
     }
 };
