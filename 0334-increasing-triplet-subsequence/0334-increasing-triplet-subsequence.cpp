07b91ca3855c8040cddc4039f9d3b8e7b1a2fd37
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-26 18:06:48 +0530

    Time: 0 ms (100%), Space: 115.7 MB (59.11%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,20 +1,13 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int first = INT_MAX, second = INT_MAX;
-        
-        for (int num : nums) {
-            if (num <= first) {
-                first = num;  // new smallest
-            } else if (num <= second) {
-                second = num; // num is bigger than first but smaller than second
-            } else {
-                // num > first and num > second → triplet exists
-                return true;
-            }
+        int first = INT_MAX , second = INT_MAX;
+        for(int num : nums){
+            if(num <= first){
+                first = num;
+            }else if(num <= second){
+                second = num;
+            }else return true;
         }
         return false;
     }
