989825cdd350c61285584f9f3020d69580681a6e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-26 17:33:04 +0530

    Time: 0 ms (100%), Space: 9.8 MB (30.11%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if (str1 + str2 != str2 + str1) {
-            return "";
-        }
-        
-        // Find gcd of lengths
-        int g = gcd((int)str1.size(), (int)str2.size());
-        
-        return str1.substr(0, g);
+        //base case
+        if(str1 + str2 != str2 + str1)return "";
+        int ans = gcd((int)str1.size() , (int)str2.size());
+        return str1.substr(0 , ans);
     }
 };
