b4032b0e80d9c1ec02bea7c1c6b20ec5ddf6dc8d
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-26 17:37:50 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.87%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -4,11 +4,11 @@ public:
         vector<bool>ans;
         int maxi = *max_element(candies.begin() , candies.end());
         for(int i = 0 ; i < candies.size() ; i++){
-            if(candies[i] + extraCandies >= maxi){
+            if(candies[i] +  extraCandies >= maxi){
                 ans.push_back(true);
             }else ans.push_back(false);
         }
         return ans;
-        
     }
+
 };
