67eb6f976bb709171c7144a52de38428b621ed6a
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-27 22:41:17 +0530

    Time: 0 ms (100%), Space: 22.1 MB (60.97%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,34 +1,31 @@
-#include <vector>
-#include <stack>
-#include <cmath>
-
 class Solution {
 public:
-    std::vector<int> asteroidCollision(std::vector<int>& asteroids) {
-        std::stack<int> asteroidStack;
-        
-        for (int asteroid : asteroids) {
-            if (asteroid > 0) { // Moving to the right
-                asteroidStack.push(asteroid);
-            } else { // Moving to the left
-                while (!asteroidStack.empty() && asteroidStack.top() > 0 && asteroidStack.top() < std::abs(asteroid)) {
-                    asteroidStack.pop();
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+        for(int a : asteroids ){
+            //if +ve :- right direction
+            if(a > 0){
+                s.push(a);
+
+            }else{ // -ve :- left direction
+                // when satck is non empty && stack have lesser value then remove it  
+                while(!s.empty() && s.top() > 0 && s.top() < abs(a)){
+                    s.pop();
                 }
-                
-                if (asteroidStack.empty() || asteroidStack.top() < 0) {
-                    asteroidStack.push(asteroid);
-                } else if (asteroidStack.top() == std::abs(asteroid)) {
-                    asteroidStack.pop();
-                }
+                // if  stack is empty or s have -ve value 
+                if(s.empty() || s.top() < 0)s.push(a);
+                // both have same value with opp direction :- both will be distroy
+                else if(s.top() == abs(a))s.pop();
+
             }
         }
-        
-        std::vector<int> result(asteroidStack.size());
-        for (int i = asteroidStack.size() - 1; i >= 0; --i) {
-            result[i] = asteroidStack.top();
-            asteroidStack.pop();
+        vector<int>ans;
+        while(!s.empty()){
+            ans.push_back(s.top());
+            s.pop();
+
         }
-        
-        return result;
+        reverse(ans.begin() , ans.end());
+        return ans;
     }
 };
