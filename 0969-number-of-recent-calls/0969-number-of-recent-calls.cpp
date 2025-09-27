40d2c57c31f2a9938f47249530604704e3a4c9d0
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-27 22:48:25 +0530

    Time: 15 ms (69.07%), Space: 64.2 MB (66.87%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
@@ -1,24 +1,21 @@
-#include <queue>
-
 class RecentCounter {
-private:
-    std::queue<int> requests;
-
+    queue<int>q;
 public:
     RecentCounter() {
         
     }
     
     int ping(int t) {
-        // Add the current timestamp to the queue
-        requests.push(t);
+        q.push(t);
+        while(!q.empty() && q.front() < t - 3000)q.pop();
+        return q.size();
         
-        // Remove all requests older than 3000 milliseconds
-        while (!requests.empty() && requests.front() < t - 3000) {
-            requests.pop();
-        }
-        
-        // Return the number of requests within the last 3000 milliseconds
-        return requests.size();
     }
+    
 };
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
