bebf30512147d188b333757555edbf9131582de1
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-27 22:59:00 +0530

    Time: 5 ms (28.81%), Space: 11.5 MB (52.29%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,38 +1,20 @@
-#include <queue>
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
     string predictPartyVictory(string senate) {
+        queue<int> r , d;
         int n = senate.size();
-        queue<int> radiantQueue;
-        queue<int> direQueue;
-        
-        // Initialize the queues with indices of the senators
-        for (int i = 0; i < n; ++i) {
-            if (senate[i] == 'R') {
-                radiantQueue.push(i);
-            } else {
-                direQueue.push(i);
-            }
+        for(int i = 0 ; i < senate.size() ; i++){
+            if(senate[i] == 'R')r.push(i);
+            else d.push(i);
         }
-        
-        // Simulate the voting process
-        while (!radiantQueue.empty() && !direQueue.empty()) {
-            int rIndex = radiantQueue.front();
-            int dIndex = direQueue.front();
-            radiantQueue.pop();
-            direQueue.pop();
-            
-            if (rIndex < dIndex) {
-                radiantQueue.push(rIndex + n); // Radiant bans Dire, re-enqueue Radiant
-            } else {
-                direQueue.push(dIndex + n); // Dire bans Radiant, re-enqueue Dire
-            }
+        while(!r.empty() && !d.empty()){
+            int rindex = r.front();
+            int dindex = d.front();
+            r.pop();
+            d.pop();
+            if(rindex < dindex)r.push(rindex + n);
+            else d.push(dindex + n);
         }
-        
-        return radiantQueue.empty() ? "Dire" : "Radiant";
+        return r.empty() ? "Dire" : "Radiant";
     }
 };
