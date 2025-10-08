dfacddb1706342730d719f3815c66851582058ab
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-10-08 17:16:11 +0530

    Time: 7 ms (72.37%), Space: 22.9 MB (43.24%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -4,21 +4,13 @@
 class Solution {
 public:
     int minEatingSpeed(std::vector<int>& piles, int h) {
-        int left = 1; // Minimum possible eating speed
-        int right = *std::max_element(piles.begin(), piles.end()); // Maximum possible eating speed
-
+        int left = 1; 
+        int right = *max_element(piles.begin(), piles.end()); 
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) {
-                // If it's possible to finish within h hours with eating speed mid or lower, try smaller eating speeds
-                right = mid;
-            } else {
-                // If it's not possible to finish within h hours with eating speed mid, try higher eating speeds
-                left = mid + 1;
-            }
+            if (canFinish(piles, h, mid)) right = mid;
+            else  left = mid + 1; 
         }
-
-        // At the end of the binary search, left and right converge to the minimum eating speed that allows Koko to finish within h hours
         return left;
     }
 
@@ -26,9 +18,8 @@ private:
     bool canFinish(const std::vector<int>& piles, int h, int k) {
         int hours = 0;
         for (int bananas : piles) {
-            // Calculate hours needed to finish eating bananas at speed k for current pile
             hours += (bananas + k - 1) / k;
         }
-        return hours <= h; // Return true if Koko can finish within h hours
+        return hours <= h; 
     }
 };
