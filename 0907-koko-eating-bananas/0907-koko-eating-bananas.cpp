619b380e41d3d82c07dfc492a7098041b1e703bf
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-10-09 00:08:40 +0530

    Time: 7 ms (72.37%), Space: 23 MB (43.24%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,25 +1,21 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int minEatingSpeed(std::vector<int>& piles, int h) {
-        int left = 1; 
-        int right = *max_element(piles.begin(), piles.end()); 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) right = mid;
-            else  left = mid + 1; 
+    bool canfinish(vector<int>& piles, int h , int k){
+        int hour = 0;
+        for(int bananas : piles){
+            hour += (bananas + k - 1)/k;
         }
-        return left;
+        return hour <= h;
     }
-
-private:
-    bool canFinish(const std::vector<int>& piles, int h, int k) {
-        int hours = 0;
-        for (int bananas : piles) {
-            hours += (bananas + k - 1) / k;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start = 1 ;
+        int end = *max_element(piles.begin() , piles.end());
+        while(start < end){
+            int mid = start + ( end - start)/2;
+            if(canfinish(piles , h , mid)){
+                end = mid;
+            }else start = mid + 1 ;
         }
-        return hours <= h; 
+        return start;
     }
 };
