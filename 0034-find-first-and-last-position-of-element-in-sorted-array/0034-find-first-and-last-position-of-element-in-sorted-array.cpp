b2282cb8314d4c4bf589e12135d74793fdbf9d0c
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-10-08 16:17:32 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,49 +1,33 @@
 class Solution {
-    private:
-    int firstocc(vector<int>& nums, int target){
-        int start = 0 , end = nums.size() - 1;
-        int ans = -1;
-        while(start <= end){
-            int mid = start + ( end - start)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                end = mid - 1;
-
-            }else if(nums[mid] > target){
-                end = mid - 1;
-
-            }else {
-                start = mid + 1;
-            }
-
-        }
-        return ans;
-    }
-
-      int lastocc(vector<int>& nums, int target){
-        int start = 0 , end = nums.size() - 1;
-        int ans = -1;
-        while(start <= end){
-            int mid = start + ( end - start)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                start = mid + 1;
-
-            }else if(nums[mid] > target){
-                end = mid - 1;
-
-            }else {
-                start = mid + 1;
-            }
-
-        }
-        return ans;
-    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-     vector<int> ans(2) ;
-     ans[0] = firstocc(nums , target );
-     ans[1] = lastocc(nums , target);  
-     return ans;
+        int  s= 0 , e = nums.size() -1;
+        vector<int>ans;
+        int f = -1  , l = -1;
+        while(s <= e ){
+            int m = s + (e-s)/2;
+            if(nums[m] == target){
+                f = m;
+                e = m - 1 ;
+            }else if(nums[m] > target){
+                e = m - 1 ;
+            }else s = m + 1 ;
+        }
+        s = 0 , e = nums.size()-1;
+
+        while(s <= e ){
+            int m = s + (e-s)/2;
+            if(nums[m] == target){
+                l = m;
+                s = m + 1 ;
+            }else if(nums[m] > target){
+                e = m - 1 ;
+            }else s = m + 1 ;
+        }
+        ans.push_back(f);
+        ans.push_back(l);
+        return ans;
+
+
     }
 };
