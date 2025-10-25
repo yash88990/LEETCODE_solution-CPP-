7e8ab4dc8d659dc7f48f7df0ef055783f1466847
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-10-25 09:21:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,23 +10,8 @@
  * };
  */
 class Solution {
-    private:
-    void inorder(TreeNode* root , vector<int> &result){
-        //base case 
-        if(root == NULL)return;
-        // left node right
-        //1.go to the left side first
-        inorder(root->left , result);
-        //2. insert the node (root element) in ans
-        result.push_back(root->val);
-        //3. got to the right side 
-        inorder(root->right , result);
-    }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> result;
-        inorder(root , result);
-        return result;
-
+        
     }
 };
