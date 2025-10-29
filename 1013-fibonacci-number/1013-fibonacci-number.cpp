4b8e89cc89ef5cc60e4f371b6bdacffefba338d1
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-10-29 21:50:10 +0530

    Time: 3 ms (65.6%), Space: 8.2 MB (16.67%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    int solve(int n){
+    int solve(int n , vector<int>&dp){
         if(n == 0 || n ==  1)return n;
-        return solve(n-1) + solve(n-2);
+        //step 3 :- if ans is present -> return 
+        if(dp[n] != -1)return dp[n];
+        //step 2 :- store in dp 
+    
+        return dp[n] = solve(n-1 , dp) + solve(n-2 , dp);
     }
     int fib(int n) {
-        return solve(n);
+
+        // step 1 
+        vector<int>dp(n+1 , -1);
+        return solve(n , dp);
         
     }
 };
