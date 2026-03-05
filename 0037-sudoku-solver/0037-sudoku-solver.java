22174e85aa40ce8f11e555eb26d649de60560394
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-03-05 21:31:40 +0530

    Time: 388 ms (8.55%), Space: 43 MB (32.32%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,34 @@
+class Solution {
+     boolean isSafe(int row , int col , char[][] board, char ch){
+        //row
+        for(int i = 0 ; i < 9 ; i++){
+             if(board[row][i] == ch)return false;
+            if(board[i][col] == ch)return false;
+            int boxrow = 3 * (row / 3 ) + i / 3;
+            int boxcol = 3 * (col / 3 ) + i % 3;
+             if(board[boxrow][boxcol] == ch)return false;
+        }
+        return true;
+    }
+    boolean solve(char[][] board){
+        for(int row = 0 ; row < 9 ; row++){
+            for(int col = 0 ; col < 9 ; col++){
+                if(board[row][col] == '.'){
+                    for(char ch = '1'  ; ch <= '9' ; ch++){
+                        if(isSafe(row , col , board , ch)){
+                            board[row][col] = ch;
+                            if(solve(board))return true;
+                            board[row][col] = '.'; // backtracking
+                        }
+                    }
+                    return false;
+                    
+                }
+            }
+        }
+        return true;
+    }
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+}
