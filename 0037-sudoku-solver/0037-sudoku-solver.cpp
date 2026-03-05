64e70729dc069e21f2c8e4436daa1f73396f8851
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-03-05 21:29:52 +0530

    Time: 382 ms (31.46%), Space: 8.8 MB (69.5%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<vector<char>>& board, char val) {
-        for (int i = 0; i < 9; i++) {
-            // check row
-            if (board[row][i] == val) return false;
-            // check column
-            if (board[i][col] == val) return false;
-            // check 3x3 box
-            int boxRow = 3 * (row / 3) + i / 3;
-            int boxCol = 3 * (col / 3) + i % 3;
-            if (board[boxRow][boxCol] == val) return false;
+    bool isSafe(int row , int col , vector<vector<char>>&board, char ch){
+        //row
+        for(int i = 0 ; i < 9 ; i++){
+             if(board[row][i] == ch)return false;
+            if(board[i][col] == ch)return false;
+            int boxrow = 3 * (row / 3 ) + i / 3;
+            int boxcol = 3 * (col / 3 ) + i % 3;
+             if(board[boxrow][boxcol] == ch)return false;
         }
         return true;
     }
-
-    bool solve(vector<vector<char>>& board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') {
-                    for (char ch = '1'; ch <= '9'; ch++) {
-                        if (isSafe(row, col, board, ch)) {
+    bool solve(vector<vector<char>> &board){
+        for(int row = 0 ; row < 9 ; row++){
+            for(int col = 0 ; col < 9 ; col++){
+                if(board[row][col] == '.'){
+                    for(char ch = '1'  ; ch <= '9' ; ch++){
+                        if(isSafe(row , col , board , ch)){
                             board[row][col] = ch;
-                            if (solve(board)) return true;
-                            board[row][col] = '.'; 
+                            if(solve(board))return true;
+                            board[row][col] = '.'; // backtracking
                         }
                     }
-                    return false;  
+                    return false;
+                    
                 }
             }
         }
-        return true;  
+        return true;
     }
-
     void solveSudoku(vector<vector<char>>& board) {
         solve(board);
     }
