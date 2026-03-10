f6dd9a3757118f4e60d4a015a86dc96b97a8d09e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-03-10 22:34:41 +0530

    Time: 0 ms (100%), Space: 18 MB (21.88%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-         int s = 0 , e = nums.size() -1;
-        
-        while(s <= e ){
+        int s = 0 , e = nums.size()-1;
+        while(s <= e){
             int m = s + ( e - s )/2;
-            
-
             if(nums[m] == target)return true;
-            else if(nums[s] == nums[m] && nums[m] == nums[e]){
-                s++;
-                e--;
-                continue;
+            else if(nums[s] == nums[m] && nums[m] == nums[e] ){
+                s++;e--;continue;
             }
+            //which part is sorted
+            else if(nums[s] <= nums[m]){ // left part sorted hai
+               if(target >= nums[s] && target <= nums[m]){
+                 e = m-1;
+               }else s = m + 1;
 
+            }else{// right part sorted hai
+               if(target >= nums[m] && target <= nums[e] ){
+                s = m + 1;
+               }else e = m- 1;
 
-            else if(nums[m] >= nums[s]){//left part sorted hai
-                 if(target >= nums[s] && target <= nums[m]){
-                    e = m - 1 ;
-                 }else s = m + 1;
-                 
-            }else { // right part sorted h
-                  if(target >= nums[m] &&  target <= nums[e]){
-                    s =  m +1  ;
-                  }else e = m -1;
             }
         }
         return false;
