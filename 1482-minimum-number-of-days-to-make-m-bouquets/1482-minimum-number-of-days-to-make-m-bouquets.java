9f568e45f9a4273efb96b9497fa8d31c98ffa993
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-03-12 22:57:12 +0530

    Time: 15 ms (96.97%), Space: 82.8 MB (41.85%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,35 @@
 class Solution {
-
-    public boolean canMake(int[] bloomDay, int m, int k, int day) {
-        int flowers = 0;
-        int bouquets = 0;
-
-        for (int bloom : bloomDay) {
-            if (bloom <= day) {
-                flowers++;
-
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
+    public boolean solve(int[] bloomDay, int m, int k, int mid) {
+        int f = 0;
+        int b = 0;
+        for(int bloom : bloomDay){
+            if(bloom <= mid){
+                f++;
+                if(f == k){
+                    b++;
+                    f = 0;
                 }
-            } else {
-                flowers = 0;
-            }
+            }else f = 0;
         }
-
-        return bouquets >= m;
+        return b >= m;
     }
-
     public int minDays(int[] bloomDay, int m, int k) {
-
         int n = bloomDay.length;
-
-        if ((long)m * k > n) return -1;
-
+        int ans = -1;
+        if(((long) m * k) > n)return -1;
         int left = Integer.MAX_VALUE;
         int right = Integer.MIN_VALUE;
-
-        for (int bloom : bloomDay) {
-            left = Math.min(left, bloom);
-            right = Math.max(right, bloom);
+        for(int num : bloomDay){
+            if(num < left)left = num;
+            if(num > right)right = num;
         }
-
-        int ans = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (canMake(bloomDay, m, k, mid)) {
+        while( left <= right){
+            int mid = left + ( right - left)/2;
+            if(solve(bloomDay , m , k , mid)){
                 ans = mid;
                 right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+            }else left = mid + 1 ;
         }
-
         return ans;
     }
 }
