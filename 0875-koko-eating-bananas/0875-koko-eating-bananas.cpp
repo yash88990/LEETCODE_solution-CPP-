366006c271c012a2924b5957862cbb43b9f90960
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-03-12 22:12:41 +0530

    Time: 14 ms (19.35%), Space: 39.5 MB (5.11%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool caneat(vector<int>piles , int h , int mid){
+        int hour = 0;
+        for(int banana : piles){
+            hour += (banana + mid - 1 )/mid;
+        }
+        return hour <= h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start = 1 , end = *max_element(piles.begin() , piles.end());
+        while( start < end){
+            int mid = start + ( end - start)/2;
+            if(caneat(piles , h , mid)){
+                end = mid;
+            }else start = mid + 1 ;
+        }
+        return start;
+    }
+};
