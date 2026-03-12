6179e5347caedaec841c8a8b32ccbac4ccb65c9d
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-03-12 22:17:27 +0530

    Time: 6 ms (99.65%), Space: 48.1 MB (29.94%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,19 @@
 class Solution {
-
-    public boolean canfinish(int[] piles, int h, int k){
+    public boolean caneat(int[] piles ,int h , int mid){
         int hour = 0;
-
-        for(int bananas : piles){
-            hour += (bananas + k - 1) / k;
+        for(int i = 0 ; i < piles.length ; i++){
+            hour += (piles[i] + mid - 1) / mid;
         }
-
         return hour <= h;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int start = 1;
-
-        int end = 0;
-        for(int p : piles){
-            end = Math.max(end, p);
+        int start = 1 , end = Integer.MIN_VALUE;
+        for(int num : piles)if(num > end)end = num;
+        while( start < end){
+            int mid = start + ( end - start)/2;
+            if(caneat(piles , h , mid))end = mid;
+            else start = mid + 1 ;
         }
-
-        while(start < end){
-            int mid = start + (end - start) / 2;
-
-            if(canfinish(piles, h, mid)){
-                end = mid;
-            } else {
-                start = mid + 1;
-            }
-        }
-
         return start;
     }
 }
