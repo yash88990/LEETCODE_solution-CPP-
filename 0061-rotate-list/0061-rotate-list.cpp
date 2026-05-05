634f6770bc19bb058f075199b76c53ad04827ea3
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-05-05 08:31:54 +0530

    Time: 194 ms (0.79%), Space: 16.5 MB (31.2%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,28 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ /*
+ m sbse phle calculate kr rha hun ki 
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next || k == 0 ) return head;
-        //linked list ka length 
-        ListNode* curr = head ;
-        int length = 1;
+        if(k==0){return head;}  //agr k=0 mtlb no rotation
+       int size = fillq(head); //loop for getting size
+       if(size==0 || size==1){return head;} //agr size 0 to kya rotate kroge
+       while(k>=size){k=k-size;} //to reduce k
+       if(k==0){return head;}
 
-        while(curr->next){
-            curr = curr->next;
-            length++;
+       rot(head,head,k);
+
+       return head;
+    }
+
+    void rot(ListNode* &head,ListNode* &l,int k)
+    {
+       static int c=0;
+       if(head!=nullptr)
+       {
+           rot(head->next,l,k);
+           if(c<k){
+           ListNode* t=new ListNode(head->val);
+           t->next=l;
+           l=t;   
+           head=nullptr;c++;}
+       }
+       else{c=0;} //taki static c ka effect na aye
+    }
+
+    int fillq(ListNode* head)
+    {   int count=0;
+        while(head!=nullptr)
+        {
+            head=head->next;
+            count++;
         }
-        k = k % length ;
-        if(k ==0 ) return head;
-        curr->next = head ;
-        int stepToNewHead = length - k ;
-        ListNode* newTail = head;
-        for(int i = 1 ; i < stepToNewHead ; i++){
-            newTail = newTail->next;
-        }
-        ListNode* newHead = newTail->next;
-        newTail->next = NULL;
-        return newHead;
+        return count;
     }
 };
