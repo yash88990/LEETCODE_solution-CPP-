3d5c19f9d867dc85b79fa09b957630e63814d290
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-09-20 00:41:06 +0530

    Time: 0 ms (100%), Space: 10.5 MB (72.82%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int la = ay2 - ay1;
-        int lb = by2 - by1;
-        int wa = ax2 - ax1;
-        int wb =  bx2 - bx1;
-        int aa = la * wa;
-        int ab = lb * wb;
-        int ol = max( 0 , min(ay2 , by2) - max(ay1 , by1));
-        int ow = max( 0 , min(ax2 , bx2) - max(ax1 , bx1));
-        int oa = ol * ow;
-        int ans = aa + ab - oa;
-        return ans;
+        int la= ay2-ay1;
+        int wa= ax2-ax1;
+        int aa= la * wa;
+
+        int lb= by2-by1;
+        int wb= bx2-bx1;
+        int ab= lb * wb;
+
+        int lo= max(0 , min(ay2,by2) - max(ay1,by1));
+        int wo= max(0 , min(ax2 , bx2) - max(ax1 , bx1));
+        int ao= lo * wo;
+
+        int totalans = aa + ab - ao;
+        return totalans;
     }
 };
