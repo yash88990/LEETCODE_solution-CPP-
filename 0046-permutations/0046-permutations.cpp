e2a430d961718bca64a3f4f1cd91e377a9c4cdad
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-09-22 21:29:33 +0530

    Time: 0 ms (100%), Space: 11.3 MB (15.71%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> result={{}};
-        for (int num : nums) {
-            vector<vector<int>> next;
-            for (auto perm : result) {
-                for (int i = 0; i <= perm.size(); i++) {
-                    vector<int> temp = perm;
-                    temp.insert(temp.begin() + i, num);
+        vector<vector<int>> result ={{}};
+        for(int num : nums){
+            vector<vector<int>>next;
+            for(auto perm : result){
+                for(int i = 0 ; i <= perm.size() ; i++ ){
+                    vector<int>temp = perm;
+                    temp.insert(temp.begin() + i , num);
                     next.push_back(temp);
                 }
             }
-            result = next;
+            result=next;
         }
-
         return result;
     }
 };
