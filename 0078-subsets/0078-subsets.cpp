7a0f1fffd1272260c55d5497765ac4f27ef67d24
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-09-23 00:07:16 +0530

    Time: 3 ms (27.4%), Space: 10.3 MB (28.35%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-
-        // Start with one empty subset
-        vector<vector<int>> result = {{}};
-
-        // Take each number one by one
-        for (int num : nums) {
-
-            // Store the new subsets we create
-            vector<vector<int>> next;
-
-            // Go through every existing subset
-            for (auto subset : result) {
-
-                // Make a copy of the current subset
-                vector<int> temp = subset;
-
-                // Add the current number to the subset
+        vector<vector<int>> result={{}};
+        for(int num:nums){
+            vector<vector<int>>next;
+            for(auto subset : result){
+                vector<int>temp=subset;
                 temp.push_back(num);
-
-                // Store the new subset
                 next.push_back(temp);
             }
-
-            // Add all new subsets to the existing result
-            for (auto subset : next) {
-                result.push_back(subset);
-            }
+            for(auto subset : next)result.push_back(subset);
         }
-
-        // Return all possible subsets
         return result;
     }
 };
