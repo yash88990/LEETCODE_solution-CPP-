0fc41a2ba381388ea09362e1bae3b314a891b7ea
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-09-25 01:05:19 +0530

    Time: 18 ms (84.07%), Space: 70.6 MB (48.84%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool solve(vector<int>& bloomDay, int m, int k, int mid) {
-        int f = 0;
-        int b = 0;
+    bool canmake(vector<int>& bloomDay, int m, int k , int day){
+        int flower = 0 , bouquets = 0;
         for(int bloom : bloomDay){
-            if(bloom <= mid){
-                f++;
-                if(f == k){
-                    b++;
-                    f = 0;
+            if(bloom <= day){
+                flower++;
+                if(flower == k){
+                    bouquets++;
+                    flower = 0;
                 }
-            }else f = 0;
+            }else{
+                flower = 0;
+            }
         }
-        return b >= m;
+        return bouquets >= m ;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        int ans = -1;
-        if((1LL * m * k) > n)return -1;
+        if((long long)m * k > bloomDay.size())return -1;
         int left = *min_element(bloomDay.begin() , bloomDay.end());
         int right = *max_element(bloomDay.begin() , bloomDay.end());
-        while( left <= right){
+        int ans= -1;
+        while(left <= right){
             int mid = left + ( right - left)/2;
-            if(solve(bloomDay , m , k , mid)){
-                ans = mid;
-                right = mid - 1;
+            if(canmake(bloomDay , m , k , mid)){
+                ans = mid ;
+                right = mid - 1 ;
             }else left = mid + 1 ;
         }
         return ans;
