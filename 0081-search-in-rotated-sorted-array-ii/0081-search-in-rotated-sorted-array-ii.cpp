34daf15076837238a80211562a084ac2061b41a2
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2026-09-24 22:34:06 +0530

    Time: 0 ms (100%), Space: 17.9 MB (98.84%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int s = 0 , e = nums.size()-1;
-        while(s <= e){
-            int m = s + ( e - s )/2;
-            if(nums[m] == target)return true;
-            else if(nums[s] == nums[m] && nums[m] == nums[e] ){
-                s++;e--;continue;
+        int start = 0 , end = nums.size()-1 ;
+        while(start <= end){
+            int mid = start + ( end - start)/2;
+            if(nums[mid] == target)return true;
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+                continue;
             }
-            //which part is sorted
-            else if(nums[s] <= nums[m]){ // left part sorted hai
-               if(target >= nums[s] && target <= nums[m]){
-                 e = m-1;
-               }else s = m + 1;
-
-            }else{// right part sorted hai
-               if(target >= nums[m] && target <= nums[e] ){
-                s = m + 1;
-               }else e = m- 1;
+            else if(nums[start] <= nums[mid]){//left part is sorted
+                //is target lies in this part
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid - 1 ;
+                }else start = mid + 1 ;
+            }else{//right part is sorted
+                 if(target >= nums[mid] && target <= nums[end]){
+                    start = mid + 1 ;
+                 }else end = mid - 1 ;
 
             }
         }
